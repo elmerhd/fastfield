b10323cccfe49be37658243ccebaab1abb9abbfb
Elmer Duron <elmerduron2016@gmail.com>
2020-01-17 10:34:05 +0800

    test classes

---
+++ src/test/java/open/elmerhd/fastfield/test/ImageGetterTest.java
@@ -0,0 +1,47 @@
+package open.elmerhd.fastfield.test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+import open.elmerhd.fastfield.ImageGetter;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author elmerhd
+ */
+public class ImageGetterTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(ImageGetter.class);
+    }
+
+    @Test
+    public void usernameTest() {
+        Response response = target("image/").queryParam("username", "").request().get(Response.class);
+        Response.ResponseBuilder expectedResponse = Response.status(Response.Status.EXPECTATION_FAILED);
+        Assert.assertTrue(expectedResponse.build().getStatus() == response.getStatus());
+    }
+    @Test
+    public void passwordTest() {
+        Response response = target("image/").queryParam("username", "").queryParam("password", "").request().get(Response.class);
+        Response.ResponseBuilder expectedResponse = Response.status(Response.Status.EXPECTATION_FAILED);
+        Assert.assertTrue(expectedResponse.build().getStatus() == response.getStatus());
+    }
+    @Test
+    public void fileTest() {
+        Response response = target("image/").queryParam("username", "").queryParam("password", "").queryParam("file", "").request().get(Response.class);
+        Response.ResponseBuilder expectedResponse = Response.status(Response.Status.EXPECTATION_FAILED);
+        Assert.assertTrue(expectedResponse.build().getStatus() == response.getStatus());
+    }
+    @Test
+    public void InvalidUsernameOrPasswordTest() {
+        Response response = target("image/").queryParam("username", "elmerhd").queryParam("password", "mypassword").queryParam("file", "file.jpg").request().get(Response.class);
+        Response.ResponseBuilder expectedResponse = Response.status(Response.Status.UNAUTHORIZED);
+        System.out.println(response.getStatus());
+        Assert.assertTrue(expectedResponse.build().getStatus() == response.getStatus());
+    }
+}
